cde1068ac95394ca75d3946bae47b92236eeb1f2
Ananth Kannan (Coach AK) <15877864+akannan1087@users.noreply.github.com>
2025-08-01 14:06:35 -0500

    added some defects

--- MyWebApp/src/main/java/com/example/service/HelloService.java
+++ MyWebApp/src/main/java/com/example/service/HelloService.java
@@ -4,4 +4,9 @@ class HelloService {
     public String sayHello(String name) {
         return "Hello, " + name + "!";
     }
+
+    public void printLength(String str) {
+        System.out.println(str.length()); // SonarQube: Possible null pointer dereference
+    }
+
 }
